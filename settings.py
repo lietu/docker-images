c453bf41e2633bd83f40dd7fe1a10dfd89ef7f6e
Janne Enberg <janne.enberg@lietu.net>
2021-06-23 01:19:09 +0300

    Triggering workflow maybe

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ from pydantic import BaseSettings
 # Any of these can be overridden via environment variables with matching names
 class Settings(BaseSettings):
     # User or organization name, used for "user/image" -name generation
-    DOCKER_USER = "digitallivinginternational"
+    DOCKER_USER = "digitallivinginternational"  # Hopefully we'll get a shorter one
 
 
 conf = Settings()
