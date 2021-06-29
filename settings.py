a66e1b87b7bf68a73181d36b72d40959daa563ee
Denis <fbjorn@users.noreply.github.com>
2021-06-29 12:24:20 +0300

    Add ubuntu-python-nginx image (#2)
    * Add ubuntu-python-nginx image
    * Add more pre-commit hooks
    * Add validation CI workflow

--- settings.py
+++ settings.py
@@ -6,5 +6,8 @@ class Settings(BaseSettings):
     # User or organization name, used for "user/image" -name generation
     DOCKER_USER = "digitallivinginternational"  # Hopefully we'll get a shorter one
 
+    # List of images that should be built beforehand
+    PRIORITY_BUILDS = ["python-base/ubuntu20.04-python3.9"]
+
 
 conf = Settings()
