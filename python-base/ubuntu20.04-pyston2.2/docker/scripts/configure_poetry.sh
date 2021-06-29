399def43683b292163df02f33e6005f5eacd4e18
Janne Enberg <janne.enberg@lietu.net>
2021-06-29 22:02:13 +0300

    Base image with Pyston (Py3.8 compatible) because why not

---
+++ python-base/ubuntu20.04-pyston2.2/docker/scripts/configure_poetry.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# shellcheck disable=SC2039
+set -exuo pipefail
+
+su "${USER}" -c ". ${POETRY_HOME}/env; poetry config virtualenvs.in-project false"
+su "${USER}" -c ". ${POETRY_HOME}/env; poetry config virtualenvs.path ${WORKON_HOME}"
