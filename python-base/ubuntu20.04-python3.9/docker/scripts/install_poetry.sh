a904f7d7ae6ba230c5ec310532fdedba3e7f6169
Joakim Nordling <joakim.nordling@gmail.com>
2022-09-05 14:43:28 +0300

    Update the Ubuntu 20.04 based python images to use poetry 1.2.0

--- python-base/ubuntu20.04-python3.9/docker/scripts/install_poetry.sh
+++ python-base/ubuntu20.04-python3.9/docker/scripts/install_poetry.sh
@@ -3,23 +3,14 @@
 # shellcheck disable=SC2039
 set -exuo pipefail
 
-POETRY_URL="https://raw.githubusercontent.com/python-poetry/poetry/${POETRY_VERSION}/get-poetry.py"
+POETRY_URL="https://install.python-poetry.org"
 
 mkdir "${POETRY_HOME}"
 chown -R "${USER}":"${GROUP}" "${POETRY_HOME}"
 
-su "${USER}" -c "curl -sSL ${POETRY_URL} -o /tmp/get-poetry.py"
-su "${USER}" -c "python /tmp/get-poetry.py --version ${POETRY_VERSION}"
+su "${USER}" -c "curl -sSL ${POETRY_URL} | python - --version ${POETRY_VERSION}"
+# Create the env file manually; installers prior to 1.2.0 created this by default
+su "${USER}" -c "echo \"export PATH=\\\"${POETRY_HOME}/bin:\\\$PATH\\\"\" > \"${POETRY_HOME}/env\""
 
 chmod +x "${POETRY_HOME}"/bin/*
 bash /src/docker/scripts/configure_poetry.sh
-
-# Remove useless old vendor packages
-cd /usr/local/poetry/lib/poetry/_vendor/
-KEEP="py${PYTHON_VERSION}"
-for dir in py*; do
-  if [[ ! $KEEP =~ (^|[[:space:]])$dir($|[[:space:]]) ]]; then
-    rm -rf "${dir}"
-  fi
-done
-cd -
