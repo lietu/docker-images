63f69061d0e9ecc1f68c317d6a767f015a02b67a
Joakim Nordling <joakim.nordling@gmail.com>
2022-09-06 15:25:15 +0300

    Update the Ubuntu 22.04 based python images to use poetry 1.2.0

--- python-base/ubuntu22.04-python3.10/docker/scripts/install_python.sh
+++ python-base/ubuntu22.04-python3.10/docker/scripts/install_python.sh
@@ -6,10 +6,12 @@ set -exuo pipefail
 # Install python
 apt-get install -y --no-install-recommends \
   "python${PYTHON_VERSION}" \
+  "python${PYTHON_VERSION}-venv" \
   python3-distutils
 
 # Set python active
 update-alternatives --install /usr/bin/python python "/usr/bin/python${PYTHON_VERSION}" 10
+update-alternatives --install /usr/bin/python3 python3 "/usr/bin/python${PYTHON_VERSION}" 10
 
 # Install Pip
 curl https://bootstrap.pypa.io/get-pip.py | python
