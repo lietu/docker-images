03e0c2f8178950b7717b561252452ea825986266
Janne Enberg <janne.enberg@lietu.net>
2023-03-07 23:42:27 +0200

    Ok fine Pyston is slow and I don't use it

--- python-base/ubuntu20.04-pyston2.2/docker/scripts/configure_poetry.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# shellcheck disable=SC2039
-set -exuo pipefail
-
-su "${USER}" -c ". ${POETRY_HOME}/env; poetry config virtualenvs.in-project false"
-su "${USER}" -c ". ${POETRY_HOME}/env; poetry config virtualenvs.path ${WORKON_HOME}"
-su "${USER}" -c ". ${POETRY_HOME}/env; poetry config experimental.new-installer false"
