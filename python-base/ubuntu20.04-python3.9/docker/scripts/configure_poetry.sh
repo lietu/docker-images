f672228c8b7d4187b54385838c4d47822cbd63f9
Joakim Nordling <joakim.nordling@ioxio.com>
2023-05-26 16:05:37 +0300

    Clean up commented out code that is incompatible with poetry 1.5.0 (#34)
    I believe the commented out code was related to this: https://github.com/python-poetry/poetry/issues/3336#issuecomment-831789763
    
    The code has been in commented out state for a long time and poetry 1.5.0 does no longer even support the option, so better clean up this old junk.

--- python-base/ubuntu20.04-python3.9/docker/scripts/configure_poetry.sh
+++ python-base/ubuntu20.04-python3.9/docker/scripts/configure_poetry.sh
@@ -5,4 +5,3 @@ set -exuo pipefail
 
 su "${USER}" -c ". ${POETRY_HOME}/env; poetry config virtualenvs.in-project false"
 su "${USER}" -c ". ${POETRY_HOME}/env; poetry config virtualenvs.path ${WORKON_HOME}"
-#su "${USER}" -c ". ${POETRY_HOME}/env; poetry config experimental.new-installer false"
