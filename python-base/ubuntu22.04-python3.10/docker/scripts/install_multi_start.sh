06d41fa7d3c0f70391b1b413ffdd4f7f1f54a84c
fbjorn <denis@fbjorn.cc>
2023-07-03 12:44:37 +0300

    Fix multi-start for api user (#44)

--- python-base/ubuntu22.04-python3.10/docker/scripts/install_multi_start.sh
+++ python-base/ubuntu22.04-python3.10/docker/scripts/install_multi_start.sh
@@ -8,5 +8,5 @@ python3.10 -m pip install --user -U pipx
 /root/.local/bin/pipx ensurepath
 export PATH="/root/.local/bin:$PATH"
 
-# Install multi-start
-pipx install multi-start
+# Install multi-start and make it available for all users
+PIPX_HOME=/opt/pipx PIPX_BIN_DIR=/usr/local/bin pipx install multi-start
