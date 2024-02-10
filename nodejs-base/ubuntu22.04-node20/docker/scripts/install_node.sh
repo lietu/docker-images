5ae40b91d6b6c5b45d83f0e5be9f3dbc35b6182c
Janne Enberg <janne.enberg@lietu.net>
2024-02-10 14:04:50 +0200

    ubuntu22.04-node20

---
+++ nodejs-base/ubuntu22.04-node20/docker/scripts/install_node.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# shellcheck disable=SC2039
+set -exuo pipefail
+
+# Set up node
+NODE_MAJOR=20
+
+# Prepare nodesource keyring
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+
+# Set up repository
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" > /etc/apt/sources.list.d/nodesource.list
+
+# Install Node
+apt-get update
+apt-get install -y --no-install-recommends nodejs
+
+# Install sane package managers
+npm install -g pnpm yarn
